aacf9cfd596832aa7ed57b64c06ec79c78c7b87f
Stefan Huber <sh@signalwerk.ch>
2022-10-26 18:37:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 noise()
-  .pixelate(20,3)
-  .scrollY(-.1)
+  .pixelate(20, 3)
+  .scrollY(-0.1)
   .modulatePixelate(
-  	noise(
-      () => ((time/6)%7)+3 
-    )
-  	.rotate( 
-      () => (time/7)%360 
-    )
-  	.scale(1, [1,1.1].ease('easeInOutCubic').fast(5),1)
-    ,200,100)
-  .out(o0)
-
-
+    noise(4)
+      .rotate(() => (time / 7) % 360)
+      .scale(1, [1, 1.05].ease("easeInOutCubic").fast(3), 1),
+    200,
+    100
+  )
+  .out()
